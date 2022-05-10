969a400312e93523fdf1154088ba0ceeeb7b562f
HirotakaUchishiba <hirotaka1999821@gmail.com>
2022-05-10 16:37:09 +0900

    first commit

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answerNumber = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if answerNumber == 0 {
+                Text("これからジャンケンをします")
+            } else if answerNumber == 1 {
+                Image("gu")
+                     .resizable()
+                     .aspectRatio(contentMode: .fit)
+                Text("グー")
+            } else if answerNumber == 2 {
+                Image("choki")
+                     .resizable()
+                     .aspectRatio(contentMode: .fit)
+                Text("チョキ")
+            } else {
+                Image("pa")
+                     .resizable()
+                     .aspectRatio(contentMode: .fit)
+                Text("パー")
+            }
+            Button("ジャンケンをする") {
+                print("たっぷされたよ！")
+                answerNumber = Int.random(in: 1...3)
+            }
+        }
     }
 }
 
